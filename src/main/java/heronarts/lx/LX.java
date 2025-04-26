f09bb9efe7a61c3b3bdab16e0673e6abbac9ff2e
Justin Belcher <jkbelcher@gmail.com>
2025-04-25 18:08:13 -0600

    TE version 1.0.1-TE.4-SNAPSHOT

--- src/main/java/heronarts/lx/LX.java
+++ src/main/java/heronarts/lx/LX.java
@@ -70,7 +70,7 @@ import com.google.gson.stream.JsonWriter;
  */
 public class LX {
 
-  public static final String VERSION = "1.0.1.TE.3-SNAPSHOT";
+  public static final String VERSION = "1.0.1-TE.4-SNAPSHOT";
 
   public static class InstantiationException extends Exception {
 
