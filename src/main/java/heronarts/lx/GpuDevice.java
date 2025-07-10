7e1da20af48677bb865a55298b1cef3404201db5
Justin Belcher <jkbelcher@gmail.com>
2025-07-10 09:15:03 -0700

    Use interface to distinguish CPU vs GPU devices for buffer handling

---
+++ src/main/java/heronarts/lx/GpuDevice.java
@@ -0,0 +1,8 @@
+package heronarts.lx;
+
+/**
+ * A marker interface designating patterns/effects that use GPU mixer mode when available.
+ * CPU buffer operations can be suppressed for these devices.
+ */
+public interface GpuDevice {
+}
