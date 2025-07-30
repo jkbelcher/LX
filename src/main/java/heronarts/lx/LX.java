158b0b72809922c404343ff6a0c01ddb2c2e5fe8
Justin Belcher <jkbelcher@gmail.com>
2025-07-30 08:46:28 -0600

    Version 1.1.1-TE.4.GPU-SNAPSHOT

--- src/main/java/heronarts/lx/LX.java
+++ src/main/java/heronarts/lx/LX.java
@@ -70,7 +70,7 @@ import com.google.gson.stream.JsonWriter;
  */
 public class LX {
 
-  public static final String VERSION = "1.1.1-TE.3.GPU-SNAPSHOT";
+  public static final String VERSION = "1.1.1-TE.4.GPU-SNAPSHOT";
 
   public static class InstantiationException extends Exception {
 
